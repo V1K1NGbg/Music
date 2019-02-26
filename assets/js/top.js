cc4752c7c221011e6cc6689c487719b5acfbfa4f
Victor <victor@ilchev.com>
2019-02-26 22:20:46 +0200

    Update 3.0 fixes
    +back button fix
    +custom cursor
    +better examples

--- assets/js/top.js
+++ assets/js/top.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 150) {        // If page is scrolled more than 50px
+    if ($(this).scrollTop() >= 250) {        // If page is scrolled more than 50px
         $('#return-to-top').fadeIn(200);    // Fade in the arrow
     } else {
         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
