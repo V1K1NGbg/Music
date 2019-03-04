9448792ffc3a81de2ccb117d693f9cb59d6795ee
Victor <victor@ilchev.com>
2019-03-04 21:59:41 +0200

    Update 4.0
    +quiz

---
+++ other pages/quiz.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+  var questionNumberDisplay = $("#questionNumber"),
+    scoreDisplay = $("#score"),
+    atemptsDisplay = $("#atempts"),
+    questionNumber = 0,
+    score = 0,
+    atempts = 3;
+
+  var quiz = $("#quiz1");
+
+  function render() {
+    questionNumberDisplay.text(questionNumber + 1);
+    scoreDisplay.text(score);
+    atemptsDisplay.text(atempts);
+    if (atempts > 0 && questions.length >= questionNumber + 1) {
+      quiz.loadTemplate($("#quiz-template"), questions[questionNumber]);
+    } else {
+      $("#quiz_result").hide();
+      quiz.loadTemplate($("#quiz-finish-template"), { score: score });
+    }
+  }
+
+  function start() {
+    quiz.off("click");
+    $("#quiz_result").show();
+    render();
+
+    quiz.click(e => {
+      var answer = e.target.id[1];
+      if (answer != "u" && answer == questions[questionNumber].correct) {
+        score += 200;
+      } else {
+        atempts = atempts - 1;
+      }
+
+      questionNumber++;
+      render();
+    });
+  }
+
+  quiz.loadTemplate($("#quiz-begin-template"));
+  quiz.click(start);
+});
